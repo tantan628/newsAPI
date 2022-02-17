634f075142ef992b273489f52c1b83612ef94d5b
tantan628 <holttanis@gmail.com>
2022-02-17 12:23:05 +0000

    reformat connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,14 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
 const config =
   ENV === 'production'
     ? {
@@ -10,13 +18,5 @@ const config =
         },
       }
     : {};
-
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
 
 module.exports = new Pool(config);
